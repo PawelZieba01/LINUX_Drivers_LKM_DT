7d059a5a0cf8e71a46038c17b3d018bb58863493
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-13 16:55:05 +0000

    Dodano kod sterownika SPI dla przetwornika DAC MCP4921
    Dodano funkcje DAC_MCP4921_Set() oraz DAC_MCP4921_Set_mV()

--- mcp4921_driver.c
+++ mcp4921_driver.c
@@ -2,11 +2,47 @@
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
 
+static struct spi_device * dac_mcp4921_dev;
+
+void DAC_MCP4921_Set(unsigned int voltage_12bit)
+{
+    #define MCP4921_CFG_BITS (0x03 << 12)                //Kanal A, Unbuffered Vref, Gain=1, DAC Enable
+    #define DATA_BUF_LEN 2
+
+    unsigned int data = MCP4921_CFG_BITS | (voltage_12bit & 0x0fff);
+    unsigned char spi_buff[DATA_BUF_LEN];
+    
+    spi_buff[0] = ( (data & 0xff00) >> 8 );
+    spi_buff[1] = ( data & 0x00ff );
+
+    pr_info("Send buffer: %x %x", spi_buff[0], spi_buff[1]);
+
+    spi_write(dac_mcp4921_dev, spi_buff, DATA_BUF_LEN);
+}
+
+
+void DAC_MCP4921_Set_mV(unsigned int voltage_mV)
+{
+    #define BINARY_VAL_FOR_1mV 0x9ee0       /* unsigned Q1.15 -> 1,2412     (1mV to wartość 1,2412 dla przetwornika) */
+    unsigned int voltage_12bit = ( ((unsigned long)voltage_mV * (unsigned long)BINARY_VAL_FOR_1mV)>>15 );    /* Konwersja Q16.15 do Q16.0 */
+    
+    pr_info("Set voltage to %d [mV]\n", voltage_mV);
+    pr_info("Set voltage to %d [12bit]\n", voltage_12bit);
+    
+    DAC_MCP4921_Set(voltage_12bit);
+}
+
+
+
 
 
 static int mtm_probe(struct spi_device *dev)
 {
         dev_info(&dev->dev, "SPI DAC Driver Probed\n");
+
+        dac_mcp4921_dev = dev;
+        DAC_MCP4921_Set_mV(1400);
+
         return 0;
 }
 
