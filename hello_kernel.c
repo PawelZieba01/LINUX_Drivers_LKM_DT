897b6eb2b129f2f75eb948934f92d0703284d5f4
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-20 12:22:46 +0000

    Poprawiono styl kodu

--- hello_kernel.c
+++ hello_kernel.c
@@ -8,7 +8,7 @@ MODULE_DESCRIPTION("Prosty moduł jądra linux");
 
 
 /* Funkcja wykonywana podczas ładowania modułu do jądra linux */
-static int __init ModuleInit(void)
+static int __init on_init(void)
 {
         pr_info("Hello kernel !!!\n");
         return 0;
@@ -16,11 +16,11 @@ static int __init ModuleInit(void)
 
 
 /* Funkcja wykonywana podczas usuwania modułu z jądra linux */
-static void __exit ModuleExit(void)
+static void __exit on_exit(void)
 {
         pr_info("Goodbye kernel !!!\n"); 
 }
 
 
-module_init(ModuleInit);
-module_exit(ModuleExit);
+module_init(on_init);
+module_exit(on_exit);
