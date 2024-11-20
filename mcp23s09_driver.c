01c0b3881d587e3cf6c1be13188f6a06176804a9
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-20 14:43:52 +0000

    Poprawiono styl kodu

--- mcp23s09_driver.c
+++ mcp23s09_driver.c
@@ -7,83 +7,82 @@
 #define IO_DIR_REG 0x00
 #define IO_GPIO_REG 0x09
 
+
 static struct spi_device * io_mcp23s09_dev;
 
 
 void IO_MCP23S09_Set(unsigned char port_value)
 {
-    unsigned char spi_buff[6] = {
-        IO_WRITE_OPCODE,
-        IO_DIR_REG,
-        0x00,               /* Ustawienie pinów jako wyjścia */
-        IO_WRITE_OPCODE,
-        IO_GPIO_REG,
-        port_value
-    };
-    
-    pr_info("Set port value to %x \n", port_value);
+        unsigned char spi_buff[6] = {
+                IO_WRITE_OPCODE,
+                IO_DIR_REG,
+                0x00,
+                IO_WRITE_OPCODE,
+                IO_GPIO_REG,
+                port_value
+        };
+        
+        pr_info("Set port value to %x \n", port_value);
 
-
-    spi_write(io_mcp23s09_dev, &spi_buff[0], 3);    /* Ustawienie pinów jako wyjścia */
-    spi_write(io_mcp23s09_dev, &spi_buff[3], 3);    /* Ustawienie wartości pinów */
+        spi_write(io_mcp23s09_dev, &spi_buff[0], 3);    /* Ustawienie pinów 
+                                                           jako wyjścia */
+        spi_write(io_mcp23s09_dev, &spi_buff[3], 3);    /* Ustawienie wartości
+                                                           pinów */
 }
+
 
 unsigned char IO_MCP23S09_Get(void)
 {
-    unsigned char spi_tx_buff[5] = {
-        IO_WRITE_OPCODE,
-        IO_DIR_REG,
-        0xff,               /* Ustawienie pinów jako wejścia */
-        IO_READ_OPCODE,
-        IO_GPIO_REG
-    };
+        unsigned char spi_tx_buff[5] = {
+                IO_WRITE_OPCODE,
+                IO_DIR_REG,
+                0xff,
+                IO_READ_OPCODE,
+                IO_GPIO_REG
+        };
 
-    unsigned char spi_rx_buff[] = {0};
+        unsigned char spi_rx_buff[] = {0};
 
-    pr_info("Get port value.\n");
+        pr_info("Get port value.\n");
 
-    spi_write_then_read(io_mcp23s09_dev, &spi_tx_buff[0], 3, 0, 0);                 /* Ustawienie pinów jako wejścia */
-    spi_write_then_read(io_mcp23s09_dev, &spi_tx_buff[3], 2, &spi_rx_buff[0], 1);   /* Odczyt danych */
+        spi_write_then_read(io_mcp23s09_dev, &spi_tx_buff[0], 3, 0, 0);
+        spi_write_then_read(io_mcp23s09_dev, &spi_tx_buff[3], 2,
+                            &spi_rx_buff[0], 1);
 
-    return spi_rx_buff[0];
+        return spi_rx_buff[0];
 }
-
 
 
 /*----- Parametr modułu przeznaczony do zapisu -----*/
 static long int io_port_state = 0;
 
-static ssize_t io_port_state_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t io_port_state_store(struct device *dev,
+                                   struct device_attribute *attr,
+                                   const char *buf, size_t count)
 {
-    int res;
+        int res;
 
-    res = kstrtol(buf, 0, &io_port_state);
-    if(res)     //Jeśli wystąpi bład podczas konwersji
-    {
-        return res;
-    }
-    
-    if(io_port_state < 0x00   ||   io_port_state > 0xff)
-    {
-        return -EINVAL;
-    }
-
-    IO_MCP23S09_Set(io_port_state);
-    return count;
+        res = kstrtol(buf, 0, &io_port_state);
+        if (res)
+                return res;
+               
+        if (io_port_state < 0x00  ||  io_port_state > 0xff)
+                return -EINVAL;
+        
+        IO_MCP23S09_Set(io_port_state);
+        return count;
 }
 
 
-static ssize_t io_port_state_show(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t io_port_state_show(struct device *dev,
+                                  struct device_attribute *attr,
+                                  char *buf)
 {
-    io_port_state = (unsigned char)IO_MCP23S09_Get();
-    return sprintf(buf, "0x%02X\n", (unsigned int)io_port_state);       //format: 0x00
+        io_port_state = (unsigned char)IO_MCP23S09_Get();
+        return sprintf(buf, "0x%02X\n", (unsigned int)io_port_state);
 }
-
-
 DEVICE_ATTR_RW(io_port_state);
 /*---------------------------------------------------*/
-
-
 
 
 static int mtm_probe(struct spi_device *dev)
@@ -92,11 +91,12 @@ static int mtm_probe(struct spi_device *dev)
 
         io_mcp23s09_dev = dev;
         
-        /* Utworzenie pliku reprezentującego atrybut w przestrzeni użytkownika */
+        /* Utworzenie pliku reprezentującego atrybut urządzenia */
         device_create_file(&dev->dev, &dev_attr_io_port_state);
 
         return 0;
 }
+
 
 static int mtm_remove(struct spi_device *dev)
 {
@@ -106,15 +106,11 @@ static int mtm_remove(struct spi_device *dev)
 }
 
 
-
-
 static const struct of_device_id mtm_of_id[] = {
         { .compatible = "microchip,my_io" },
         {},
 };
-
 MODULE_DEVICE_TABLE(of, mtm_of_id); 
-
 
 
 static struct spi_driver mtm_driver = {
