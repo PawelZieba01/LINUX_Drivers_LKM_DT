cf6296784be80ea61c245ba7b3ad8e243c260b9f
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-20 12:05:06 +0000

    Poprawiono formatowanie kodu

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ obj-m += hello_kernel.o
 # Zmienna M= wskazuje ścieżkę, gdzie mają zostać umieszczone pliki wynikowe operacji budoania modułu
 
 all:
-		make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 
 clean:
-		make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- hello_kernel.c
+++ hello_kernel.c
@@ -1,27 +1,26 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-/*
-* Metadane
-*/
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Paweł Zięba  AGH UST  2024");
 MODULE_DESCRIPTION("Prosty moduł jądra linux");
 
-/* Funkcja wywoływana podczas ładowania modułu do jądra linux */
+
+/* Funkcja wykonywana podczas ładowania modułu do jądra linux */
 static int __init ModuleInit(void)
 {
-    printk("Hello kernel !!!\n");
-    return 0;
+        pr_info("Hello kernel !!!\n");
+        return 0;
 }
+
 
 /* Funkcja wykonywana podczas usuwania modułu z jądra linux */
 static void __exit ModuleExit(void)
 {
-    printk("Goodbye kernel !!!\n"); 
+        pr_info("Goodbye kernel !!!\n"); 
 }
 
 
 module_init(ModuleInit);
 module_exit(ModuleExit);
-
