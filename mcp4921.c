5720569eaa36302deea8890391bebb77676e1a0c
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-23 09:43:42 +0000

    Poprawiono nazwy plików i prefixy nazw w kodzie

--- mcp4921.c
+++ mcp4921.c
@@ -9,10 +9,10 @@
                                         dla przetwornika) */
 
 
-static struct spi_device *dac_mcp4921_dev;
+static struct spi_device *mcp4921_spi_dev;
 
 
-void DAC_MCP4921_Set(unsigned int voltage_12bit)
+void mcp4921_set(unsigned int voltage_12bit)
 {        
         unsigned int data = MCP4921_CFG_BITS | (voltage_12bit & 0x0fff);
         unsigned char spi_buff[2];
@@ -22,11 +22,11 @@ void DAC_MCP4921_Set(unsigned int voltage_12bit)
 
         pr_info("Send buffer: %x %x", spi_buff[0], spi_buff[1]);
 
-        spi_write(dac_mcp4921_dev, spi_buff, 2);
+        spi_write(mcp4921_spi_dev, spi_buff, 2);
 }
 
 
-void DAC_MCP4921_Set_mV(unsigned int voltage_mV)
+void mcp4921_set_mV(unsigned int voltage_mV)
 {
          /* Konwersja Q16.15 do Q16.0 */
         unsigned int voltage_12bit = (((unsigned long)voltage_mV *
@@ -35,71 +35,69 @@ void DAC_MCP4921_Set_mV(unsigned int voltage_mV)
         pr_info("Set voltage to %d [mV]\n", voltage_mV);
         pr_info("Set voltage to %d [12bit]\n", voltage_12bit);
 
-        DAC_MCP4921_Set(voltage_12bit);
+        mcp4921_set(voltage_12bit);
 }
 
 
 /*----- Parametr modułu przeznaczony do zapisu -----*/
-static long int dac_voltage_mV = 0;
+static long int mcp4921_voltage_mV = 0;
 
-static ssize_t dac_voltage_mV_store(struct device *dev,
+static ssize_t mcp4921_voltage_mV_store(struct device *dev,
                                     struct device_attribute *attr,
                                     const char *buf, size_t count)
 {
         int res;
 
-        res = kstrtol(buf, 0, &dac_voltage_mV);
+        res = kstrtol(buf, 0, &mcp4921_voltage_mV);
         if (res)
                 return res;
         
-        if (dac_voltage_mV < 0  ||  dac_voltage_mV > 3300)
+        if (mcp4921_voltage_mV < 0  ||  mcp4921_voltage_mV > 3300)
                 return -EINVAL;
         
-        DAC_MCP4921_Set_mV(dac_voltage_mV);
+        mcp4921_set_mV(mcp4921_voltage_mV);
         return count;
 }
 
-DEVICE_ATTR_WO(dac_voltage_mV);
+DEVICE_ATTR_WO(mcp4921_voltage_mV);
 /*---------------------------------------------------*/
 
 
-static int mtm_probe(struct spi_device *dev)
+static int mcp4921_probe(struct spi_device *dev)
 {
         dev_info(&dev->dev, "SPI DAC Driver Probed\n");
 
-        dac_mcp4921_dev = dev;
+        mcp4921_spi_dev = dev;
         
         /* Utworzenie pliku reprezentującego atrybut urządzenia */
-        device_create_file(&dev->dev, &dev_attr_dac_voltage_mV);
+        device_create_file(&dev->dev, &dev_attr_mcp4921_voltage_mV);
 
         return 0;
 }
 
-static int mtm_remove(struct spi_device *dev)
+static void mcp4921_remove(struct spi_device *dev)
 {
         dev_info(&dev->dev, "SPI DAC Driver Removed\n");
-        device_remove_file(&dev->dev, &dev_attr_dac_voltage_mV);
-        return 0;
+        device_remove_file(&dev->dev, &dev_attr_mcp4921_voltage_mV);
 }
 
 
-static const struct of_device_id mtm_of_id[] = {
-        { .compatible = "microchip,my_dac" },
+static const struct of_device_id mcp4921_of_id[] = {
+        { .compatible = "microchip,mcp4921_dac" },
         {},
 };
-MODULE_DEVICE_TABLE(of, mtm_of_id); 
+MODULE_DEVICE_TABLE(of, mcp4921_of_id); 
 
 
-static struct spi_driver mtm_driver = {
-        //.id_table = my_dac;
-        .probe = (void*) mtm_probe,
-        .remove = (void*) mtm_remove,
+static struct spi_driver mcp4921_driver = {
+        .probe = mcp4921_probe,
+        .remove = mcp4921_remove,
         .driver = {
-                .name = "my_dac",
-                .of_match_table = mtm_of_id,
+                .name = "mcp4921_dac",
+                .of_match_table = mcp4921_of_id,
                 .owner = THIS_MODULE,
         },
 };
-module_spi_driver(mtm_driver);
+module_spi_driver(mcp4921_driver);
 
 MODULE_LICENSE("GPL v2");
