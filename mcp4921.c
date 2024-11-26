71dc952ba7d57dfeca2b34d5bf8f9a7dbaeeb2f6
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-26 18:57:59 +0000

    Przerobiono kod tak aby korzystał z misc_device

--- mcp4921.c
+++ mcp4921.c
@@ -1,8 +1,9 @@
 #include <linux/spi/spi.h>
 #include <linux/cdev.h>
+#include <linux/miscdevice.h>
+
 
 #define MY_DEV_NAME "dac_mcp4921"
-#define MY_CLASS_NAME "spi_dac_devices"
 #define SPI_BUS 0
 #define DAC_MCP4921_REF_VOLTAGE_mV 3300
 #define MCP4921_CFG_BITS (0x03 << 12)           /* Kanal A, Unbuffered Vref,
@@ -13,9 +14,7 @@
 #define MAX_WRITE_SIZE 10                       /* 4 znaki na liczbę i jeden 
                                                    znak Null */                                                   
 
-static struct class *my_class;
-static struct cdev my_cdev;
-static dev_t my_devt;
+
 static struct spi_device *mcp4921_spi_dev;
 
 
@@ -45,12 +44,11 @@ void mcp4921_set_mV(unsigned int voltage_mV)
 
 
 /*-------------------- Obsługa urządzenia znakowego --------------------*/
-/* Funkcja wywoływana podczas zapisywania do pliku urządzenia */
 static ssize_t mcp4921_write(struct file *filp, const char *buf,
                            size_t count, loff_t *f_pos) 
 {
         char kspace_buffer[MAX_WRITE_SIZE] = "";
-        int voltage_mV, ret_val;
+        int voltage_mV, ret;
 
         pr_info("Write to device file.\n");
 
@@ -66,10 +64,10 @@ static ssize_t mcp4921_write(struct file *filp, const char *buf,
 
         pr_info("%s\n", kspace_buffer);
 
-        ret_val = kstrtol(kspace_buffer, 0, (long*)&voltage_mV);
-        if (ret_val) {
+        ret = kstrtol(kspace_buffer, 0, (long*)&voltage_mV);
+        if (ret) {
                 pr_err("Can't convert data to integer\n");
-                return ret_val;
+                return ret;
         }
 
         if (voltage_mV < 0  ||  voltage_mV > DAC_MCP4921_REF_VOLTAGE_mV) {
@@ -83,40 +81,35 @@ static ssize_t mcp4921_write(struct file *filp, const char *buf,
 }
 
 
-/* Struktura przechowująca informacje o operacjach możliwych do
-   wykonania na pliku urządzenia */
 static struct file_operations fops = {
         .owner=THIS_MODULE,
         .write=mcp4921_write
 };
+
+
+struct miscdevice mcp4921_device = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = MY_DEV_NAME,
+    .fops = &fops,
+};
 /*----------------------------------------------------------------------*/
+
+
 
 
 static int mcp4921_probe(struct spi_device *dev)
 {
+        int res;
+        
         dev_info(&dev->dev, "SPI DAC Driver Probed\n");
 
-        /* Zapisanie wskaźnika do urządzenia SPI */
         mcp4921_spi_dev = dev;
 
-        /* Zaalokowanie numerów MAJOR i MINOR dla urządzenia */
-        alloc_chrdev_region(&my_devt, 0, 1, MY_DEV_NAME);
-
-        /* Stworzenie klasy urządzeń, widocznej w /sys/class */
-        my_class = class_create(THIS_MODULE, MY_CLASS_NAME);
-
-        /* Inicjalizacja urządzenia znakowego - podpięcie funkcji
-           do operacji na plikach (file operations) */
-        cdev_init(&my_cdev, &fops);
-
-        /* Dodanie urządzenia do systemu */
-        cdev_add(&my_cdev, my_devt, 1);
-
-        /* Utworzenie pliku w przestrzeni użytkownika (w /dev),
-           reprezentującego urządzenie */
-        device_create(my_class, NULL, my_devt, NULL, MY_DEV_NAME);
-
-        pr_info("Alocated device MAJOR number: %d\n", MAJOR(my_devt));
+        res = misc_register(&mcp4921_device);
+        if (res) {
+                pr_err("Misc device registration failed!");
+                return res;
+        }
         
         return 0;
 }
@@ -125,20 +118,11 @@ static int mcp4921_probe(struct spi_device *dev)
 static void mcp4921_remove(struct spi_device *dev)
 {
         dev_info(&dev->dev, "SPI DAC Driver Removed\n");
-
-         /* Usunięcie pliku urządzenia z przestrzeni użytkownika */
-        device_destroy(my_class, my_devt);
-
-        /* Usunięcie urządzenia z systemu */
-        cdev_del(&my_cdev);
-
-        /* Usunięcie klasy urządzenia */
-        class_unregister(my_class);
-        class_destroy(my_class);
-
-        /* Zwolnienie przypisanych numerów MAJOR i MINOR */
-        unregister_chrdev_region(my_devt, 1);
+        misc_deregister(&mcp4921_device);
+        
 }
+
+
 
 
 static const struct of_device_id mcp4921_of_id[] = {
@@ -149,7 +133,6 @@ MODULE_DEVICE_TABLE(of, mcp4921_of_id);
 
 
 static struct spi_driver mcp4921_driver = {
-        //.id_table = my_dac;
         .probe = mcp4921_probe,
         .remove = mcp4921_remove,
         .driver = {
