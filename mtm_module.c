8a01dc12ea21b3aa004797e48cda8e3709351517
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-26 22:17:08 +0000

    Poprawiono styl kodu

--- mtm_module.c
+++ mtm_module.c
@@ -4,7 +4,8 @@
 /*----- Parametr modułu przeznaczony do odczytu -----*/
 static unsigned long int mtm_param = 64;
 
-static ssize_t mtm_param_show(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t mtm_param_show(struct device *dev, struct device_attribute *attr,
+                              char *buf)
 {
         return sprintf(buf, "%lu\n", mtm_param);
 }
@@ -38,22 +39,15 @@ static int mtm_remove(struct platform_device *dev)
         return 0;
 }
 
-// static const struct platform_device_id mtm_id[] = {
-//         { .name = "mtm_device" },
-//     { }
-// };
 
 static const struct of_device_id mtm_of_id[] = {
         { .compatible = "mtm" },
         { },
 };
-
-//MODULE_DEVICE_TABLE(platform, mtm_id);        //platform - dopasowuje sterownik po nazwie
-MODULE_DEVICE_TABLE(of, mtm_of_id);             //of - dopasowuje sterownik po compatible
+MODULE_DEVICE_TABLE(of, mtm_of_id);
 
 
 static struct platform_driver mtm_driver = {
-        //.id_table = mtm_id,
         .probe = mtm_probe,
         .remove = mtm_remove,
         .driver = {
@@ -66,5 +60,3 @@ module_platform_driver(mtm_driver);
 
 
 MODULE_LICENSE("GPL v2");
-
-/* Powyższe komentarze dotyczące mechanizmu dopasowania sterowników i urządzeń dt zostaną usunięte - wykorzystano mechanizm of, dopasowujący poprzez 'compatible' */
