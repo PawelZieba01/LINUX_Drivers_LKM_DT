0ec46aef51838802c9933cde1f475e06c1cb1155
Pawel Zieba <zpawel.zieba@gmail.com>
2024-11-27 15:17:12 +0000

    Dodano modyfikację rozwiązującą problem dostępu do danych wielu urządzeń

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Ten kod przedstawia problem użycia globalnej zmiennej przez wiele urządzeń obsługiwanych przez jeden sterownik.
 
 Tworzymy dodatkowy węzeł, w Device Tree, a każde urządzenie w funkcji probe inkrementuje licznik instancji sterownika.
+
+W tej modyfikacji inkrementowany parametr znajduje się w strukturze danych sterownika `driver_data`, która została zaalokowana w pamięci dla każdego węzła (urządzenia) Device Tree.
+
+
+
+Dodać:
+
+- #include <linux/slab.h>
+- węzeł w DT
+- funkcje _show i _store
+- alokowanie pamięci
+- dev_set_drvdata(&dev->dev, dev_data); get_set_drvdata(&dev->dev);
--- mtm_module.c
+++ mtm_module.c
@@ -1,34 +1,66 @@
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
+
 
 /*----- Parametr modułu przeznaczony do odczytu i zapisu -----*/
-static unsigned long int mtm_param_instances = 0;
+struct mtm_dev_data {
+        long int param;
+};
 
 static ssize_t mtm_param_show(struct device *dev,
                               struct device_attribute *attr, char *buf)
 {
-        return sprintf(buf, "%lu\n", mtm_param_instances);
+        struct mtm_dev_data *dev_data = dev_get_drvdata(dev);
+        return sprintf(buf, "%lu\n", dev_data->param);
 }
-DEVICE_ATTR_RO(mtm_param);
+
+
+static ssize_t mtm_param_store(struct device *dev,
+                                    struct device_attribute *attr,
+                                    const char *buf, size_t count)
+{
+        int res;
+        struct mtm_dev_data *dev_data;
+
+        dev_data = dev_get_drvdata(dev);
+
+        res = kstrtol(buf, 0, &dev_data->param);
+        if (res)
+                return res;
+        return count;
+}
+DEVICE_ATTR_RW(mtm_param);
 /*------------------------------------------------------------*/
+
 
 
 static int mtm_probe(struct platform_device *dev)
 {
+        struct mtm_dev_data *dev_data;
+
         dev_info(&dev->dev, "probed\n");
 
         /* Utworzenie pliku reprezentującego atrybut */
         device_create_file(&dev->dev, &dev_attr_mtm_param);
 
-        mtm_param_instances++;
-        dev_info(&dev->dev, "mtm_param: %lu\n", mtm_param_instances);
+        dev_data = kzalloc(sizeof(struct mtm_dev_data), GFP_KERNEL);
+        dev_data->param++;
+
+        dev_set_drvdata(&dev->dev, dev_data);
+        
+        dev_info(&dev->dev, "param: %lu\n", dev_data->param);
         return 0;
 }
 
 
 static int mtm_remove(struct platform_device *dev)
 {
+        struct mtm_dev_data *dev_data = (struct mtm_dev_data *)dev->dev.driver_data;
+
         dev_info(&dev->dev, "removed\n");
+
+        kfree(dev_data);
 
         /* Usunięcie pliku reprezentującego atrybut urządzenia*/
         device_remove_file(&dev->dev, &dev_attr_mtm_param);
