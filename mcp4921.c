85af1bc7fc9f40ff9421a94abc5a183cbe77733c
Pawel Zieba <zpawel.zieba@gmail.com>
2024-12-11 14:21:54 +0000

    Dodano rezerwacje pamięci dla danych urządzenia

--- mcp4921.c
+++ mcp4921.c
@@ -3,11 +3,32 @@
 #include <linux/spi/spi.h>
 
 
+struct mcp4921_data {
+        struct spi_device *dev;
+};
+
+
 static int mcp4921_probe(struct spi_device *dev)
 {
+        struct mcp4921_data *data;
+
         dev_info(&dev->dev, "SPI DAC Driver Probed\n");
+
+        data = devm_kzalloc(&dev->dev, sizeof(struct mcp4921_data), GFP_KERNEL);
+        if (IS_ERR(data)) {
+               dev_err(&dev->dev, "Can't allocate device data\n");
+               goto err_alloc;
+        }
+        
+        data->dev = dev;
+        dev_set_drvdata(&dev->dev, data);
+
         return 0;
+
+err_alloc:
+        return -1;
 }
+
 
 static void mcp4921_remove(struct spi_device *dev)
 {
@@ -19,16 +40,7 @@ static const struct of_device_id mcp4921_of_id[] = {
         { .compatible = "microchip,mcp4921_dac" },
         {},
 };
-
-MODULE_DEVICE_TABLE(of, mcp4921_of_id);                     //platform - dopasowuje sterownik po compatible
-
-
-// static const struct spi_device_id mcp4921_dac[] = {
-//         {"mcp4921_dac", 0},
-//         {},
-// };
-
-// MODULE_DEVICE_TABLE(spi, mcp4921_dac);                    //spi - dopasowuje sterownik po nazwie urządzenia z tablicy typu spi_device_id
+MODULE_DEVICE_TABLE(of, mcp4921_of_id);
 
 
 static struct spi_driver mcp4921_driver = {
@@ -44,5 +56,3 @@ static struct spi_driver mcp4921_driver = {
 module_spi_driver(mcp4921_driver);
 
 MODULE_LICENSE("GPL v2");
-
-/* Powyższe komentarze dotyczące mechanizmu dopasowania sterowników i urządzeń dt zostaną usunięte - wykorzystano mechanizm of, dopasowujący poprzez 'compatible' */
