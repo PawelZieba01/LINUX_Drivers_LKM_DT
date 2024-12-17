d4ea91b6cd575f23596094065de56b93af88d30e
Pawel Zieba <zpawel.zieba@gmail.com>
2024-12-17 23:41:56 +0000

    Wprowadzono poprawki z dnia 16-12-2024

--- pcf8574.c
+++ pcf8574.c
@@ -1,17 +1,16 @@
 #include <linux/of.h>
-#include <linux/fs.h>
 #include <linux/i2c.h>
 #include <linux/miscdevice.h>
 #include <linux/regmap.h>
-#include <linux/uaccess.h>
 
-#define MY_DEV_NAME "pcf8574"
-#define MAX_WRITE_SIZE 10
-#define MAX_READ_SIZE 7
+#define PCF8574_WRITE_BUFF_SIZE 6
+#define PCF8574_READ_BUFF_SIZE 6
+#define PCF8574_PORT_MAX_VALUE 0xff
+#define PCF8574_PORT_MIN_VALUE 0x00
 
 
-struct pcf8574_data {
-        struct i2c_client *i2cdev;
+struct pcf8574 {
+        struct i2c_client *client;
         struct regmap *rmap;
         struct miscdevice mdev;
         int port_state;
@@ -20,42 +19,45 @@ struct pcf8574_data {
 /*------------------ Obsługa urządzenia znakowego ------------------*/
 
 static ssize_t pcf8574_write(struct file *filp, const char *buf,
-                           size_t count, loff_t *f_pos) 
+                             size_t count, loff_t *f_pos) 
 {
-        int err;
-        struct pcf8574_data *data = filp->private_data;
-        struct device *dev = &data->i2cdev->dev; 
-        char kspace_buffer[MAX_WRITE_SIZE] = "";
+        int err = 0;
+        int port_state;
+        char kbuff[PCF8574_WRITE_BUFF_SIZE] = "";
+        struct pcf8574 *pcf8574 = container_of(filp->private_data,
+                                                 struct pcf8574, mdev);
 
-        dev_info(dev, "Write to device file.\n");
+        dev_info(&pcf8574->client->dev, "Write to device file.\n");
 
-        if (count > MAX_WRITE_SIZE) {
-                dev_err(dev, "Bad input number.\n");
+        if (count > PCF8574_WRITE_BUFF_SIZE) {
+                dev_err(&pcf8574->client->dev, "Bad input number.\n");
                 return -ERANGE;
         }
 
-        err = copy_from_user(kspace_buffer, buf, count);
+        err = copy_from_user(kbuff, buf, count);
         if (err) {
-                dev_err(dev,
+                dev_err(&pcf8574->client->dev,
                         "Can't copy data from user space.\n");
                 return -EFAULT;		
         }
 
-        err = kstrtol(kspace_buffer, 0, (long*)&data->port_state);
+        err = kstrtol(kbuff, 0, (long *) &port_state);
         if (err) {
-                dev_err(dev,
+                dev_err(&pcf8574->client->dev,
                         "Can't convert data to integer.\n");
                 return err;
         }
 
-        if (data->port_state < 0x00  ||  data->port_state > 0xff) {
-                dev_err(dev, "Bad voltage value.\n");
+        if (port_state < PCF8574_PORT_MIN_VALUE  ||
+            port_state > PCF8574_PORT_MAX_VALUE) {
+                dev_err(&pcf8574->client->dev, "Bad voltage value.\n");
                 return -EINVAL;
         }
 
-        err = regmap_write(data->rmap, 0x00, data->port_state);
+        pcf8574->port_state = port_state;
+        err = regmap_write(pcf8574->rmap, PCF8574_PORT_MIN_VALUE, port_state);
         if (err) {
-                dev_err(dev, "I2C communication error.\n");
+                dev_err(&pcf8574->client->dev, "I2C communication error.\n");
                 return err;
         }
 
@@ -66,24 +68,26 @@ static ssize_t pcf8574_write(struct file *filp, const char *buf,
 static ssize_t pcf8574_read(struct file *filp, char *buf, size_t count,
                            loff_t *f_pos)
 {
-        int err;
-        struct pcf8574_data *data = filp->private_data;
-        struct device *dev = &data->i2cdev->dev; 
-        unsigned char port_buf[MAX_READ_SIZE];
+        int err = 0;
+        unsigned char port_buf[PCF8574_READ_BUFF_SIZE] = "";
+        struct pcf8574 *pcf8574 = container_of(filp->private_data,
+                                                 struct pcf8574, mdev);
+        
 
-        dev_info(dev, "Read from device file.\n");
+        dev_info(&pcf8574->client->dev, "Read from device file.\n");
 
-        err = regmap_read(data->rmap, 0xff, &data->port_state);
+        err = regmap_read(pcf8574->rmap, PCF8574_PORT_MAX_VALUE,
+                          &pcf8574->port_state);
         if (err) {
-                dev_err(dev, "I2C communication error.\n");
+                dev_err(&pcf8574->client->dev, "I2C communication error.\n");
                 return err;
         }
 
-        sprintf(port_buf, "0x%X\n", data->port_state);
+        sprintf(port_buf, "0x%X\n", pcf8574->port_state);
      
         err = copy_to_user(buf, port_buf, strlen(port_buf));
         if (err) {
-                dev_err(dev, "Can't copy data to uder space.\n");
+                dev_err(&pcf8574->client->dev,"Can't copy data to uder space.\n");
                 return -EIO;
         }
                                 
@@ -91,37 +95,11 @@ static ssize_t pcf8574_read(struct file *filp, char *buf, size_t count,
 }
 
 
-int pcf8574_open(struct inode *node, struct file *filp)
-{       
-        struct pcf8574_data *data = container_of(filp->private_data,
-                                                 struct pcf8574_data,
-                                                 mdev);
-
-        dev_info(&data->i2cdev->dev, "Driver file open.\n");
-        
-        filp->private_data = data;
-        return 0;
-}
-
-
-int pcf8574_release(struct inode *node, struct file *filp)
-{
-        struct pcf8574_data *data = filp->private_data;
-
-        dev_info(&data->i2cdev->dev, "Driver file close.\n");
-
-        filp->private_data = NULL;
-        return 0;
-}
-
-
 static struct file_operations fops = 
 {
         .owner = THIS_MODULE,
         .write = pcf8574_write,
-        .read = pcf8574_read,
-        .open = pcf8574_open,
-        .release = pcf8574_release 
+        .read = pcf8574_read
 };
 /*------------------------------------------------------------------*/
 
@@ -129,16 +107,12 @@ static struct file_operations fops =
 /*----------------------------- REGMAP -----------------------------*/
 static bool writeable_reg(struct device *dev, unsigned int reg)
 {
-        if (reg == 0x00)
-                return true;
-        return false;
+        return (reg == PCF8574_PORT_MIN_VALUE);
 }
   
 static bool readable_reg(struct device *dev, unsigned int reg)
 {
-        if (reg == 0xff)
-                return true;
-        return false;
+        return (reg == PCF8574_PORT_MAX_VALUE);
 }
 /*------------------------------------------------------------------*/
 
@@ -146,27 +120,25 @@ static bool readable_reg(struct device *dev, unsigned int reg)
 static int pcf8574_probe(struct i2c_client *client,
                          const struct i2c_device_id *id)
 {
-        int err;
-        struct pcf8574_data *data;
+        int err = 0;
+        struct pcf8574 *pcf8574;
         struct regmap_config reg_conf;
-
-        dev_info(&client->dev, "I2C IO Driver Probed.\n");
         
-        data = devm_kzalloc(&client->dev, sizeof(struct pcf8574_data),
+        /* pcf8574 data allocation */
+        pcf8574 = devm_kzalloc(&client->dev, sizeof(struct pcf8574),
                             GFP_KERNEL);
-        if (IS_ERR(data)) {
-               dev_err(&client->dev, "Can't allocate memory for device data.\n");
-               goto err_alloc;
-        }            
+        if (IS_ERR(pcf8574)) {
+               dev_err(&client->dev, "Memory allocation failed.\n");
+               err = PTR_ERR(pcf8574->rmap);
+               goto out_ret_err;
+        }         
 
-        data->mdev.minor = MISC_DYNAMIC_MINOR;
-        data->mdev.name = MY_DEV_NAME;
-        data->mdev.fops = &fops;
-        err = misc_register(&data->mdev);
-        if (err) {
-                dev_err(&client->dev, "Misc device registration failed.\n");
-                goto err_misc;
-        }
+        pcf8574->client = client;   
+        
+        /* miscdevice config */
+        pcf8574->mdev.minor = MISC_DYNAMIC_MINOR;
+        pcf8574->mdev.name = "pcf8574";
+        pcf8574->mdev.fops = &fops;
 
         /* regmap config */
         memset(&reg_conf, 0, sizeof(reg_conf));
@@ -174,33 +146,40 @@ static int pcf8574_probe(struct i2c_client *client,
         reg_conf.val_bits = 8;
         reg_conf.writeable_reg = writeable_reg;
         reg_conf.readable_reg = readable_reg;
-;
 
+        /* miscdevice init */
+        err = misc_register(&pcf8574->mdev);
+        if (err) {
+                dev_err(&client->dev, "Misc device registration failed.\n");
+                goto out_ret_err;
+        }
+        
         /* regmap init */
-        data->rmap = devm_regmap_init_i2c(client, &reg_conf);
-        if (IS_ERR(data->rmap)) {
+        pcf8574->rmap = devm_regmap_init_i2c(client, &reg_conf);
+        if (IS_ERR(pcf8574->rmap)) {
                 dev_err(&client->dev, "Can't init spi regmap.\n");
-                goto err_rmap;
+                err = PTR_ERR(pcf8574->rmap);
+                goto out_deregister_misc;
         }
 
-        data->i2cdev = client;
-        dev_set_drvdata(&client->dev, data);
+        dev_set_drvdata(&client->dev, pcf8574);
         
-        return 0;
+        dev_info(&client->dev, "I2C IO Driver Probed.\n");
+        goto out_ret_err;
 
 
-err_rmap:
-err_misc:
-err_alloc:
-        return -1;
+out_deregister_misc:
+        misc_deregister(&pcf8574->mdev);
+out_ret_err:
+        return err;
 }
 
 
 static void pcf8574_remove(struct i2c_client *client)
 {
-        struct pcf8574_data *data = dev_get_drvdata(&client->dev);
+        struct pcf8574 *pcf8574 = dev_get_drvdata(&client->dev);
+        misc_deregister(&pcf8574->mdev);
         dev_info(&client->dev, "I2C IO Driver Removed\n");
-        misc_deregister(&data->mdev);
 }
 
 
